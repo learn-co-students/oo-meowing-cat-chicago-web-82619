73b20357b85c2e38adb726ce9d3750b5bd6a965d
github username <github email address>
2019-08-27 14:07:01 -0500

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,18 @@
+require 'pry'
 ## code your solution here. 
+class Cat
+    attr_accessor :name
+# binding.pry
+
+    # def initialize(name)
+    #     @name = name
+    # end
+
+#   def name(name)
+#   name
+#   end
+    def meow
+        puts "meow!"
+    end
+
+end
